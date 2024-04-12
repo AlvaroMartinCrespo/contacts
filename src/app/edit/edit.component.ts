ef103736edc0ec60e50f67029f0d21987f994dcd
Alvaro <alvaro.martin.crespo.00@gmail.com>
2024-04-12 19:44:49 +0200

    init

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-edit',
   standalone: true,
   imports: [],
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.css'
+  styleUrl: './edit.component.css',
 })
 export class EditComponent {
-
+  id: string = '';
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    // Get the id from the url
+    this.id = this.route.snapshot.params['id'];
+  }
 }
