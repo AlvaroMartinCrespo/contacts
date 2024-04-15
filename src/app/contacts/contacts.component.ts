ea895d1aa0abd6b0b2ea03bcc5897f58ec840a98
Alvaro <alvaro.martin.crespo.00@gmail.com>
2024-04-15 19:38:43 +0200

    contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.css'
+  styleUrl: './contacts.component.css',
 })
 export class ContactsComponent {
-
+  arrayContacs: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
