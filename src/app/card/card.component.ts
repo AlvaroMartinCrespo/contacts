9280cf4bfacdbb2138a3d53244638942fb8c0fff
Alvaro <alvaro.martin.crespo.00@gmail.com>
2024-04-16 10:47:50 +0200

    contacts

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -19,6 +19,7 @@ export class CardComponent {
     this.menuVisible = !this.menuVisible;
   }
 
+  @Input() id: string = '';
   @Input() title: string = '';
   @Input() content: string = '';
   @Input() tlf: string = '';
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { FormsModule } from '@angular/forms';
+import { User } from '../interfaces/user.interface';
 @Component({
   selector: 'app-contacts',
   standalone: true,
@@ -9,8 +10,41 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './contacts.component.css',
 })
 export class ContactsComponent {
-  arrayContacs: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  users: User[] | undefined;
   value: string = '';
+
+  ngOnInit(): void {
+    this.users = [
+      {
+        id: '1',
+        name: 'Alvaro1',
+        email: 'a@a.es',
+        description: 'Contenido de la tarjeta. Tiene que ser extenso1',
+        tlf: '123456789',
+      },
+      {
+        id: '2',
+        name: 'Alvaro2',
+        email: 'a@a.es',
+        description: 'Contenido de la tarjeta. Tiene que ser extenso2',
+        tlf: '123456789',
+      },
+      {
+        id: '3',
+        name: 'Alvaro3',
+        email: 'a@a.es',
+        description: 'Contenido de la tarjeta. Tiene que ser extenso3',
+        tlf: '123456789',
+      },
+      {
+        id: '4',
+        name: 'Alvaro4',
+        email: 'a@a.es',
+        description: 'Contenido de la tarjeta. Tiene que ser extenso4',
+        tlf: '123456789',
+      },
+    ];
+  }
 
   search(value: string) {
     console.log(value);
