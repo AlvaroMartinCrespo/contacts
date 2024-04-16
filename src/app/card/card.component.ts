d81327975e0ddc302a9e6543ddffa41fee520369
Alvaro <alvaro.martin.crespo.00@gmail.com>
2024-04-16 10:14:27 +0200

    card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,33 @@
 import { Component, Input } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [MenuModule, ToastModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
 export class CardComponent {
+  items: MenuItem[] | undefined;
+
+  menuVisible: boolean = false;
+
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
+
   @Input() title: string = '';
   @Input() content: string = '';
   @Input() tlf: string = '';
   @Input() email: string = '';
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Edit', url: '/edit' },
+      { label: 'Delete', url: '/delete' },
+    ];
+  }
 }
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, FormsModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css',
 })
 export class ContactsComponent {
   arrayContacs: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  value: string = '';
 }
