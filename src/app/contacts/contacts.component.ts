fb4071026d5587d94e54e358ac990ef77039030a
Alvaro <alvaro.martin.crespo.00@gmail.com>
2024-04-16 10:20:17 +0200

    input contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -11,4 +11,8 @@ import { FormsModule } from '@angular/forms';
 export class ContactsComponent {
   arrayContacs: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   value: string = '';
+
+  search(value: string) {
+    console.log(value);
+  }
 }
